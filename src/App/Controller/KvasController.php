5f57036dbda4a298c03fe1d053d843c6ddc596ce
Rekryt <rekrytkw@gmail.com>
2024-10-15 20:06:37 +0300

    fix: update KVAS format

--- src/App/Controller/KvasController.php
+++ src/App/Controller/KvasController.php
@@ -16,10 +16,10 @@ class KvasController extends TextController {
         $data = $this->request->getQueryParameter('data') ?? '';
         $handlers = [
             'domains' => fn(string $row) => 'kvas add ' . $row . ' Y',
-            'ip4' => fn(string $row) => 'kvas add ' . $row . '/32' . ' Y',
-            'ip6' => fn(string $row) => 'kvas add ' . $row . '/128' . ' Y',
-            'cidr4' => fn(string $row) => 'kvas add ' . $row . ' Y',
-            'cidr6' => fn(string $row) => 'kvas add ' . $row . ' Y',
+            'ip4' => fn(string $row) => 'kvas add ' . $row . '/32',
+            'ip6' => fn(string $row) => 'kvas add ' . $row . '/128',
+            'cidr4' => fn(string $row) => 'kvas add ' . $row,
+            'cidr6' => fn(string $row) => 'kvas add ' . $row,
         ];
         return implode($this::DELIMITER, array_map($handlers[$data] ?? fn(string $row) => $row, $response));
     }
