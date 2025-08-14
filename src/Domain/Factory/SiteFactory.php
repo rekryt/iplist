a0e024b6aeddc070fb666fec95026b7799e2a6a5
Rekryt <rekrytkw@gmail.com>
2025-08-14 10:12:17 +0300

    fix: accept ip addresses with a zero at the end

--- src/Domain/Factory/SiteFactory.php
+++ src/Domain/Factory/SiteFactory.php
@@ -104,8 +104,7 @@ class SiteFactory {
                                 !str_starts_with($item, '10.') &&
                                 !str_starts_with($item, '172.16.') &&
                                 !str_starts_with($item, '192.168.') &&
-                                !str_starts_with($item, 'fd') &&
-                                !str_ends_with($item, '.0')))
+                                !str_starts_with($item, 'fd')))
                 )
             )
         );
